9610888be6da72123f2b59310cbae95d72620e99
Oleh <herashchenko.oleh@gmail.com>
2024-06-03 12:08:58 +0300

    Reworks project build flow

--- scripts/build_engine.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-
-current_path = os.path.abspath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-build_dir_path = os.path.abspath(os.path.join(current_path, '..', 'builds/engine'))
-binaries_dir_path = os.path.abspath(os.path.join(current_path, '..', 'bin'))
-
-subprocess.call(['cmake', '--build', build_dir_path, '--config', 'Release'])
-subprocess.call(['cmake', '--install', build_dir_path, '--prefix', binaries_dir_path])
--- scripts/generate_game_project.py
+++ scripts/generate_game_project.py
@@ -1,9 +1,32 @@
 import os
 import subprocess
 import platform
+import tempfile
+import shutil
+
+def get_freetype_path(file_path, pattern):
+    with open(file_path,'r') as file:
+        for line in file:
+            if line.startswith(pattern):
+                return line.replace(pattern, '')
+    return ''
+
+def replace(file_path, pattern, subst):
+    fh, abs_path = tempfile.mkstemp()
+    with os.fdopen(fh,'w') as new_file:
+        with open(file_path) as old_file:
+            for line in old_file:
+                if line.startswith(pattern):
+                    new_file.write(pattern + subst)
+                else:
+                    new_file.write(line)
+
+    shutil.copymode(file_path, abs_path)
+    os.remove(file_path)
+    shutil.move(abs_path, file_path)
 
 current_path = os.path.abspath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-build_dir_path = os.path.abspath(os.path.join(current_path, '..', 'builds/games/SpaceShip'))
+build_dir_path = os.path.abspath(os.path.join(current_path, '..', 'builds/SpaceShip'))
 games_dir_path = os.path.abspath(os.path.join(current_path, '..', 'games/SpaceShip'))
 
 current_platform = platform.system()
@@ -12,3 +35,8 @@ if current_platform == "Darwin":
     subprocess.call(['cmake', '-B' + build_dir_path, '-S' + games_dir_path, '-G', 'Unix Makefiles', '--fresh'])
 else:
     subprocess.call(['cmake', '-B' + build_dir_path, '-S' + games_dir_path, '--fresh'])
+
+cache_file = os.path.join(build_dir_path, 'CMakeCache.txt')
+ft2build_pattern = 'FREETYPE_INCLUDE_DIR_ft2build:PATH='
+freetype_pattern = 'FREETYPE_INCLUDE_DIR_freetype2:PATH='
+replace(cache_file, ft2build_pattern, get_freetype_path(cache_file, freetype_pattern))
